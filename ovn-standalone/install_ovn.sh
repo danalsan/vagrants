29f480e0faa696b6f5afdcb334391d7b720ba1ca
Daniel Alvarez <dalvarez@redhat.com>
2019-02-21 12:42:13 +0100

    Vagrant files for OVN standalone / multinode setups

---
+++ ovn-standalone/install_ovn.sh
@@ -0,0 +1,31 @@
+sudo setenforce 0
+
+sudo sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
+
+sudo yum group install "Development Tools" -y
+sudo yum install python-devel python-six -y
+
+GIT_REPO=${GIT_REPO:-https://github.com/openvswitch/ovs}
+GIT_BRANCH=${GIT_BRANCH:-master}
+
+git clone $GIT_REPO
+cd ovs
+
+
+if [[ "z$GIT_BRANCH" != "z" ]]; then
+    git checkout $GIT_BRANCH
+fi
+
+./boot.sh
+CFLAGS="-O0 -g" ./configure --prefix=/usr
+make -j5 V=0
+sudo make install
+
+
+sudo /usr/share/openvswitch/scripts/ovs-ctl start --system-id="ovn"
+sudo /usr/share/openvswitch/scripts/ovn-ctl start_ovsdb --db-nb-create-insecure-remote=yes --db-sb-create-insecure-remote=yes
+sudo /usr/share/openvswitch/scripts/ovn-ctl start_northd
+sudo /usr/share/openvswitch/scripts/ovn-ctl start_controller
+sudo ovs-vsctl set open . external-ids:ovn-remote=unix:/usr/var/run/openvswitch/ovnsb_db.sock
+sudo ovs-vsctl set open . external-ids:ovn-encap-ip=127.0.0.1
+sudo ovs-vsctl set open . external-ids:ovn-encap-type=geneve
