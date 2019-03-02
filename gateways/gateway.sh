43d23f289d2726490ff418305d7440980042cd60
Daniel Alvarez <dalvarez@redhat.com>
2019-03-02 16:27:20 +0100

    Add Multinode and Multinode DVR scenarios

---
+++ gateways/gateway.sh
@@ -0,0 +1,43 @@
+sudo setenforce 0
+
+sudo sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
+
+sudo yum group install "Development Tools" -y
+sudo yum install python-devel python-six -y
+
+GIT_REPO=${GIT_REPO:-https://github.com/openvswitch/ovs}
+GIT_BRANCH=${GIT_BRANCH:-master}
+
+git clone $GIT_REPO
+cd ovs
+
+
+if [[ "z$GIT_BRANCH" != "z" ]]; then
+    git checkout $GIT_BRANCH
+fi
+
+./boot.sh
+CFLAGS="-O0 -g" ./configure --prefix=/usr
+make -j5 V=0 install
+sudo make install
+
+hostname=$(hostname)
+ip=${!hostname}
+
+sudo /usr/share/openvswitch/scripts/ovs-ctl start --system-id=$hostname
+sudo /usr/share/openvswitch/scripts/ovn-ctl start_controller
+
+sudo ovs-vsctl set open . external-ids:ovn-bridge=br-int
+sudo ovs-vsctl set open . external-ids:ovn-remote=tcp:192.168.50.10:6642
+sudo ovs-vsctl set open . external-ids:ovn-encap-type=geneve
+sudo ovs-vsctl set open . external-ids:ovn-encap-ip=$ip
+
+ovs-vsctl --may-exist add-br br-ex
+ovs-vsctl br-set-external-id br-ex bridge-id br-ex
+ovs-vsctl br-set-external-id br-int bridge-id br-int
+ovs-vsctl set open . external-ids:ovn-bridge-mappings=external:br-ex
+ovs-vsctl set open . external-ids:ovn-cms-options="enable-chassis-as-gw"
+
+# Add eth2 to br-ex
+ovs-vsctl add-port br-ex eth2
+
---
+++ ovn-multinode-dvr/create_ovn_resources.sh
@@ -0,0 +1,30 @@
+ovn-nbctl ls-add network1
+ovn-nbctl lsp-add network1 vm1
+ovn-nbctl lsp-set-addresses vm1 "40:44:00:00:00:01 192.168.0.11"
+ovn-nbctl lsp-add network1 vm2
+ovn-nbctl lsp-set-addresses vm2 "40:44:00:00:00:02 192.168.0.12"
+
+ovn-nbctl ls-add public
+ovn-nbctl lsp-add public public-localnet
+ovn-nbctl lsp-set-type public-localnet localnet
+ovn-nbctl lsp-set-addresses public-localnet unknown
+ovn-nbctl lsp-set-options public-localnet network_name=external
+
+ovn-nbctl lr-add router1
+ovn-nbctl lrp-add router1 router1-net1 40:44:00:00:00:03 192.168.0.1/24
+ovn-nbctl lsp-add network1 net1-router1
+ovn-nbctl lsp-set-type net1-router1 router
+ovn-nbctl lsp-set-addresses net1-router1 router
+ovn-nbctl lsp-set-options net1-router1 router-port=router1-net1
+
+ovn-nbctl lrp-add router1 router1-public 40:44:00:00:00:04 172.24.4.1/24
+ovn-nbctl lsp-add public public-router1
+ovn-nbctl lsp-set-type public-router1 router
+ovn-nbctl lsp-set-addresses public-router1 router
+ovn-nbctl lsp-set-options public-router1 router-port=router1-public
+
+ovn-nbctl --id=@gc0 create Gateway_Chassis name=public-gw1 chassis_name=gw1 priority=20 -- --id=@gc1 create Gateway_Chassis name=public-gw2 chassis_name=gw2 priority=10 -- set Logical_Router_Port router1-public 'gateway_chassis=[@gc0,@gc1]'
+
+ovn-nbctl lr-nat-add router1 snat 172.24.4.1 192.168.0.0/24
+ovn-nbctl lr-nat-add router1 dnat_and_snat 172.24.4.100 192.168.0.11 vm1 40:44:00:00:00:05
+ovn-nbctl lr-nat-add router1 dnat_and_snat 172.24.4.101 192.168.0.12 vm2 40:44:00:00:00:06
