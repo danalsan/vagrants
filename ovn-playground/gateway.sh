384e25db477499755249cc3e8ee5faad82f51ee6
Daniel Alvarez <dalvarez@redhat.com>
2019-04-24 13:27:29 +0000

    Add missing script to ovn-playground
    gateway.sh was missing from the ovn-plaground scenario
    
    Signed-off-by: Daniel Alvarez <dalvarez@redhat.com>

---
+++ ovn-playground/gateway.sh
@@ -0,0 +1,24 @@
+source /vagrant/utils/common-functions
+
+install_ovs
+
+hostname=$(hostname)
+ip=${!hostname}
+
+sudo /usr/share/openvswitch/scripts/ovs-ctl start --system-id=$hostname
+sudo /usr/share/openvswitch/scripts/ovn-ctl start_controller
+
+sudo ovs-vsctl set open . external-ids:ovn-bridge=br-int
+sudo ovs-vsctl set open . external-ids:ovn-remote=tcp:192.168.50.10:6642
+sudo ovs-vsctl set open . external-ids:ovn-encap-type=geneve
+sudo ovs-vsctl set open . external-ids:ovn-encap-ip=$ip
+
+ovs-vsctl --may-exist add-br br-ex
+ovs-vsctl br-set-external-id br-ex bridge-id br-ex
+ovs-vsctl br-set-external-id br-int bridge-id br-int
+ovs-vsctl set open . external-ids:ovn-bridge-mappings=external:br-ex
+ovs-vsctl set open . external-ids:ovn-cms-options="enable-chassis-as-gw"
+
+# Add eth2 to br-ex
+ovs-vsctl add-port br-ex eth2
+
