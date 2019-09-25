b04aad0ea2307fbdf9805f2b2b025d9fb758324e
Flavio Fernandes <flaviof@redhat.com>
2019-09-25 18:05:30 -0400

    OVN split: Fix building issues
    - OVN build depends on OVS: Clone and compile both OVS and OVN repos;
    - Fix paths changed by OVN install after breaking up from OVS.

--- ovn-multinode/central.sh
+++ ovn-multinode/central.sh
@@ -2,14 +2,14 @@
 
 source /vagrant/utils/common-functions
 
-install_ovs
+install_ovn
 
 hostname=$(hostname)
 
 /usr/share/openvswitch/scripts/ovs-ctl start --system-id=$hostname
-/usr/share/openvswitch/scripts/ovn-ctl start_ovsdb --db-nb-create-insecure-remote=yes --db-sb-create-insecure-remote=yes
-/usr/share/openvswitch/scripts/ovn-ctl start_northd
-/usr/share/openvswitch/scripts/ovn-ctl start_controller
+/usr/share/ovn/scripts/ovn-ctl start_ovsdb --db-nb-create-insecure-remote=yes --db-sb-create-insecure-remote=yes
+/usr/share/ovn/scripts/ovn-ctl start_northd
+/usr/share/ovn/scripts/ovn-ctl start_controller
 
 ovs-vsctl set open . external-ids:ovn-bridge=br-int
 ovs-vsctl set open . external-ids:ovn-remote=unix:/usr/var/run/openvswitch/ovnsb_db.sock
