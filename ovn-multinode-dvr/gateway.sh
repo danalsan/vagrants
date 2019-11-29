edc785565f840c89feb069134069b23806d923b4
Jakub Libosvar <libosvar@redhat.com>
2019-11-29 12:27:02 +0100

    ovn-multinode-dvr: Install python3 dependencies and create br-int
    The patch adds deps for Python 3 as Python 2 is no longer supported in
    OVS.
    
    It also creates br-int before setting it in ovn-controller to avoid race
    between ovn-controller creating the bridge and provisioning script
    setting attributes on the bridge.

--- ovn-multinode-dvr/gateway.sh
+++ ovn-multinode-dvr/gateway.sh
@@ -10,6 +10,10 @@ ip=${!hostname}
 /usr/share/openvswitch/scripts/ovs-ctl start --system-id=$hostname
 /usr/share/ovn/scripts/ovn-ctl start_controller
 
+# OVN controller creates the bridge but it's racey
+# with the br-set-external-id command
+ovs-vsctl add-br br-int
+
 ovs-vsctl set open . external-ids:ovn-bridge=br-int
 ovs-vsctl set open . external-ids:ovn-remote=tcp:${central}:6642
 ovs-vsctl set open . external-ids:ovn-encap-type=geneve
