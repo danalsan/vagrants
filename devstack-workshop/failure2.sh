718026548a9772af21eadee779556c24d682647e
Daniel Alvarez <dalvarez@redhat.com>
2020-10-21 15:34:30 +0200

    Fix typo on failure2 script - devstack workshop
    Signed-off-by: Daniel Alvarez <dalvarez@redhat.com>

--- devstack-workshop/failure2.sh
+++ devstack-workshop/failure2.sh
@@ -2,4 +2,4 @@
 set -x
 
 # Posion the MAC_Binding entry for the devstack IP in the FIP network
-$ for i in $(ovn-sbctl --bare --columns _uuid find mac_binding ip="172.24.4.3"); do ovn-sbctl set MAC_Binding $i mac="de\:ad\:be\:ef\:01\:01"; done
+for i in $(ovn-sbctl --bare --columns _uuid find mac_binding ip="172.24.4.3"); do ovn-sbctl set MAC_Binding $i mac="de\:ad\:be\:ef\:01\:01"; done
